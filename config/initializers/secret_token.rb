5ff97b20434c5ada3175d027c95ed735396359af
Enrique Figueroa <elder.e.figueroa@gmail.com>
2014-01-08 23:41:43 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Siphon::Application.config.secret_key_base = 'f05e3b609a13b013a9fca929ecec41c063865aa5ae3126acbd686acfc7db4babafe0cdda0d82b13d0e259ceded743f84b5169aba4fd49f857bc3a78f66646798'
