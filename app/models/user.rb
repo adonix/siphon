f84135d434ff5144b03dec294043c7494eccddae
Enrique Figueroa <elder.e.figueroa@gmail.com>
2014-01-20 14:22:42 -0800

    Add user validation for password and password confirmation, tests to user signing feature, and functionality to create new users successfully and strong parameters logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :videos
+
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 60 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  has_secure_password
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, length: { minimum: 6 }
+
+  #has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_secure_password
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,38 @@ describe "User Pages" do
     it { should have_content('Join') }
     it { should have_title('Join') }
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "join page" do
+
+    before { visit join_path }
+
+    let(:submit) { "Join"}
+
+    describe "with invalid information" do
+      it 'should not create a new user account' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "user_name",                   with: "John Doe"
+        fill_in "user_email",                  with: "jdoe@somecompany.com"
+        fill_in "user_password",               with: "foobar"
+        fill_in "user_password_confirmation",  with: "foobar"
+      end
+
+      it 'should create a new user account' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
