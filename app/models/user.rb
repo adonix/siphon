b18c654d5d98c9a69a9ca5342e7a759cd98c0c6a
Enrique Figueroa <elder.e.figueroa@gmail.com>
2014-01-24 14:37:26 -0800

    User sign and sign out functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :videos
 
+  before_create :create_remember_token
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 60 }
@@ -11,4 +12,18 @@ class User < ActiveRecord::Base
 
   #has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1::hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
